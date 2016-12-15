90bf187f899e874c30c02dda1ac3ab3e966a9d86
Sri Maneru <sriharimaneru@gmail.com>
2016-12-15 09:59:57 -0800

    Handling static files

--- yaari/yaari/urls.py
+++ yaari/yaari/urls.py
@@ -15,12 +15,16 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from django.conf.urls import include
 from django.conf.urls.static import static
 from django.conf import settings
 
+
 urlpatterns = [
     url(r'^pairs/', include('pairs.urls')),
     url(r'^admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
