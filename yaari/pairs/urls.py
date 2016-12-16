48820dcf0a74d4d4173ba01cc51dcffb86d137ae
Sri Maneru <sriharimaneru@gmail.com>
2016-12-16 00:50:25 -0800

    tweaked the urls

--- yaari/pairs/urls.py
+++ yaari/pairs/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'active/$', views.ActivePairsView.as_view()),
-    url(r'history/$', views.HistoryView.as_view()),
+    url(r'^$', views.ActivePairsView.as_view()),
+    url(r'^history/?$', views.HistoryView.as_view()),
 ]
--- yaari/yaari/urls.py
+++ yaari/yaari/urls.py
@@ -23,7 +23,7 @@ from django.conf import settings
 
 
 urlpatterns = [
-    url(r'^pairs/', include('pairs.urls')),
+    url(r'', include('pairs.urls')),
     url(r'^admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
